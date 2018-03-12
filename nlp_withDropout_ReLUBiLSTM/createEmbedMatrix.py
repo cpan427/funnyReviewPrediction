b42c304b36b7ec786987d30bbb660669638673a6
cpan427 <capan@stanford.edu>
2018-03-12 11:28:43 -0700

    Update createEmbedMatrix.py

--- nlp_withDropout_ReLUBiLSTM/createEmbedMatrix.py
+++ nlp_withDropout_ReLUBiLSTM/createEmbedMatrix.py
@@ -1,6 +1,5 @@
 """
 createEmbeddingMatrix:
-
 Pseudocode:
 - open the matrix //Note that we should just make it an embedding
    - make this into a dictionary
@@ -9,17 +8,19 @@ Pseudocode:
    - create a random embedding
 - else:
    - append the word embedding
-
-
 //Read the thing into a numpy array by doing genfromtxt(__, delimiter=',')
 """
+#Note that this would have to be run on python3
 
 import numpy as np
 import pandas as pd
 
-gloveEmbed = open("glove.6B.50d.txt", 'r', encoding='utf8')
+print ("Opening Glove pretrained vectors")
+gloveEmbed = open("data/glove.6B.50d.txt", 'r', encoding='utf8')
 embedDict = {}
 embed = gloveEmbed.readlines()
+
+print ("Creating dictionary")
 for r in embed:
 	r = r.split(" ")
 	r[len(r)-1] = r[len(r)-1][:-1]
@@ -28,18 +29,21 @@ for r in embed:
 	r = [float(x) for x in r]
 	embedDict[val] = r
 
-vocab = open("words.txt", 'r')
+vocab = open("data/small/words.txt", 'r')
 matrix = []
 
+print ("Done with glove")
 for w in vocab.readlines():
-	curr = w[:-1]
+	curr = w
+	if w[-1] == "\n":
+		curr = w[:-1]
 	currArr = None
 	if curr in embedDict:
 		currArr = embedDict[curr]
 	else:
-		#print ("We tried....")
 		currArr = [0.0 for i in range(50)]
 	matrix.append(currArr)
-#print (matrix)
+print ("Writing the file")
+
 res = np.asarray(matrix)
-np.savetxt("embeddingMatrix.csv", res, delimiter=",")
+np.savetxt("data/small/embeddings.csv", res, delimiter=",")
